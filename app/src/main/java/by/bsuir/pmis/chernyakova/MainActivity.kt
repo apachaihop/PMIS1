a23d0eede8d5dc54f644c6d2f16d5576add977b6
heruga <olga4ernyakova@gmail.com>
2023-10-15 17:03:16 +0300

    Commit for Shishka

--- app/src/main/java/by/bsuir/pmis/chernyakova/MainActivity.kt
+++ app/src/main/java/by/bsuir/pmis/chernyakova/MainActivity.kt
@@ -3,9 +3,11 @@ package by.bsuir.pmis.chernyakova
 import android.content.res.Resources
 import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,21 +17,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,15 +63,28 @@ class MainActivity : ComponentActivity() {
             Lab_0Theme {
                 // A surface container using the 'background' color from the theme
                 Scaffold(
+                    bottomBar = {
+                        BottomAppBar(
+                            containerColor = MaterialTheme.colorScheme.background,
+                            contentColor = Color.White,
+                        ) {
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                textAlign = TextAlign.Center,
+                                text = stringResource(id = R.string.version)
+                            )
+                        }
+                    },
                     topBar = {
                         TopAppBar(
                             colors = topAppBarColors(
                                 containerColor = MaterialTheme.colorScheme.background,
-                                titleContentColor = Pink40,
+
                             ),
                             title = {
 
-                                Text("About bOOkabu")
+                                Text("About Ebooks")
                             },
                             navigationIcon = {
                                 IconButton(onClick = { /* do something */ }) {
@@ -98,15 +125,132 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 @Composable
 fun AboutScreen(){
-Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(15.dp)) {
-    Image(modifier=Modifier.size(30.dp),painter = painterResource(id = R.drawable.info),contentDescription = null)
     Column {
-        Text(text="Info", color= Pink40)
-        Text (text= stringResource(R.string.info), color= Pink40, fontSize = 12.sp)
+
+
+        Image( modifier = Modifier.size(90.dp).align(Alignment.CenterHorizontally),painter = painterResource(id = R.drawable.info), contentDescription = null)
+        Text(modifier = Modifier
+            .fillMaxWidth().padding(top=15.dp),
+            textAlign = TextAlign.Center,
+            text = "Artwork by heruga", fontSize = 19.sp)
+        Text(modifier = Modifier
+            .fillMaxWidth().padding( bottom = 10.dp,top=20.dp),
+            textAlign = TextAlign.Center,
+            text = "Our networks",fontSize = 19.sp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        )
+        {
+            Image(
+
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.webbrowser),
+                contentDescription = null
+            )
+            Image(
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.telegram),
+                contentDescription = null
+            )
+            Image(
+                modifier = Modifier.size(50.dp).clickable {  },
+                painter = painterResource(id = R.drawable.instagram),
+                contentDescription = null
+            )
+        }
+        TextButton(onClick = { /*TODO*/ },) {
+
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.mail),
+                    contentDescription = null
+                )
+                Column {
+                    Text(text = "Email",color= Color.White, fontSize = 16.sp)
+                    Text(text = stringResource(R.string.email),color=Color.LightGray, fontSize = 14.sp)
+                }
+
+            }
+        }
+        TextButton(onClick = { /*TODO*/ },) {
+
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.information),
+                    contentDescription = null
+                )
+                Column {
+                    Text(text = "FAQ",color=Color.White, fontSize = 16.sp)
+                    Text(text = stringResource(R.string.FAQ),color=Color.LightGray, fontSize = 14.sp)
+                }
+
+            }
+        }
+        TextButton(onClick = { /*TODO*/ },) {
+
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.starrating),
+                    contentDescription = null
+                )
+                Column {
+                    Text(text = "Rate us",color=Color.White, fontSize = 16.sp)
+                    Text(text = stringResource(R.string.rate),color=Color.LightGray, fontSize = 14.sp)
+                }
+
+            }
+        }
+        TextButton(onClick = { /*TODO*/ },) {
+
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                Image(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(id = R.drawable.statistics),
+                    contentDescription = null
+                )
+                Column {
+                    Text(text = "Allow us to collect analytics",color=Color.White, fontSize = 16.sp)
+                    Text(text = stringResource(R.string.analytics),color= Color.LightGray, fontSize = 14.sp)
+                }
+                var checked by remember { mutableStateOf(true) }
+
+                Switch(
+                    modifier=Modifier.padding(start = 70.dp),
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                    }
+                )
+
+            }
+        }
+
     }
-
-
-}
 
 }
 @Preview(showBackground = true)
